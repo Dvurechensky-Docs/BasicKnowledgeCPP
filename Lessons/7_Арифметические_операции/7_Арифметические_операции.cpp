56022ab2914fc5af1d3e532054c811786ec8cfcb
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-05-12 02:36:41 +0300

    1.1

--- Lessons/7_Арифметические_операции/7_Арифметические_операции.cpp
+++ Lessons/7_Арифметические_операции/7_Арифметические_операции.cpp
@@ -1,4 +1,12 @@
-﻿// 7_Арифметические_операции.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+﻿/*
+ * Author: Nikolay Dvurechensky
+ * Site: https://www.dvurechensky.pro/
+ * Gmail: dvurechenskysoft@gmail.com
+ * Last Updated: 12 мая 2025 02:26:45
+ * Version: 1.0.3
+ */
+
+// 7_Арифметические_операции.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
 //
 
 #include <iostream>
