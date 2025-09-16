30ffcd8bb0cc4cd6106979063b6bb51aa2c7f9da
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-16 06:51:33 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-16

--- Lessons/7_Арифметические_операции/7_Арифметические_операции.cpp
+++ Lessons/7_Арифметические_операции/7_Арифметические_операции.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 сентября 2025 06:51:31
- * Version: 1.0.25
+ * Last Updated: 16 сентября 2025 06:51:33
+ * Version: 1.0.26
  */
 
 // 7_Арифметические_операции.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
