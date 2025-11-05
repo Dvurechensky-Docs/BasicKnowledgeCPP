302237febe972b28b7c9d3682f656034be16ded1
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-05 06:51:28 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-05

--- Lessons/6_Ввод_данных/6_Ввод_данных.cpp
+++ Lessons/6_Ввод_данных/6_Ввод_данных.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 ноября 2025 06:51:28
- * Version: 1.0.76
+ * Last Updated: 05 ноября 2025 06:51:28
+ * Version: 1.0.77
  */
 
 // 6_Ввод_данных.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
