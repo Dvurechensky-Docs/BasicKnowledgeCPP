e155bdb710ad49f2326dac384d94befe7676dbde
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-11 09:13:50 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-11

--- Lessons/1_Lesson_Первая_программа/1_Lesson_Первая_программа.cpp
+++ Lessons/1_Lesson_Первая_программа/1_Lesson_Первая_программа.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 февраля 2026 10:33:01
- * Version: 1.0.174
+ * Last Updated: 11 февраля 2026 09:13:49
+ * Version: 1.0.175
  */
 
 // 1_Lesson_Первая_программа.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
