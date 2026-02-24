75cbd174c26409bd6b85700f9019fe755568e499
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-24 13:22:00 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-24

--- Lessons/4_Переменные/4_Переменные.cpp
+++ Lessons/4_Переменные/4_Переменные.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 февраля 2026 14:22:15
- * Version: 1.0.187
+ * Last Updated: 24 февраля 2026 13:21:59
+ * Version: 1.0.188
  */
 
 // 4_Переменные.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
