a5200716a7eb79c408d0ab283255cf8d3a1f7f94
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-19 06:51:35 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-19

--- Lessons/3_Lesson_Типы_данных/3_Lesson_Типы_данных.cpp
+++ Lessons/3_Lesson_Типы_данных/3_Lesson_Типы_данных.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 марта 2026 06:51:36
- * Version: 1.0.209
+ * Last Updated: 19 марта 2026 06:51:34
+ * Version: 1.0.210
  */
 
 // 3_Lesson_Типы_данных.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
