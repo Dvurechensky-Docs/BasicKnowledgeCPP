77ec06331b298d70fc028e69cede3be2143b9ec2
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-07 10:55:39 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-07

--- Lessons/2_Lesson_Escape_последовательности/2_Lesson_Escape_последовательности.cpp
+++ Lessons/2_Lesson_Escape_последовательности/2_Lesson_Escape_последовательности.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 06 апреля 2026 12:47:44
- * Version: 1.0.229
+ * Last Updated: 07 апреля 2026 10:55:39
+ * Version: 1.0.230
  */
 
 // 2_Lesson_Escape_последовательности.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
--- Lessons/6_Ввод_данных/6_Ввод_данных.cpp
+++ Lessons/6_Ввод_данных/6_Ввод_данных.cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 06 апреля 2026 12:47:44
- * Version: 1.0.229
+ * Last Updated: 07 апреля 2026 10:55:39
+ * Version: 1.0.230
  */
 
 // 6_Ввод_данных.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
