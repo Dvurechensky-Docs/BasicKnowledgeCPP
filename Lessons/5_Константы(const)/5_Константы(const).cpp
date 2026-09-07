5f0a6d175055650e096f94b92d8d65e91a26229c
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-09-07 08:14:55 +0300

    Auto-update headers via pre_commit.ps1 - 2026-09-07

--- Lessons/5_Константы(const)/5_Константы(const).cpp
+++ Lessons/5_Константы(const)/5_Константы(const).cpp
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 06 сентября 2026 11:06:48
- * Version: 1.0.385
+ * Last Updated: 07 сентября 2026 08:14:54
+ * Version: 1.0.386
  */
 
 // 5_Константы(const).cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
